6a0441644f01c6ae57ce808fdbf685cf84548e66
Wahab Qureshi <wahab.qureshi@cwponlinesolutions.com>
2019-12-02 16:42:55 +0000

    Revert accidental changes

--- tests/app/index.php
+++
@@ -1,8 +0,0 @@
-<html lang="en">
-<body>
-This is a sample page for behat test. Run on localhost:8000
-<div id="debugBar">
-    <div class="message">Requested service 'AccountService' was not found.</div>
-</div>
-</body>
-</html>
