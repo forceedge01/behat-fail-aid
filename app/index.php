866c184c1cef963407679e172579657463c8091e
Abdul Qureshi <abdul.qureshi@digital.justice.gov.uk>
2021-02-15 17:58:43 +0000

    Sonar fix

--- app/index.php
+++ app/index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE>
 <html lang="en">
-<title>Test page</title>
+<head>
+	<title>Test page</title>
+</head>
 <body>
 This is a sample page for behat test. Run on localhost:8531
 <div id="debugBar">
